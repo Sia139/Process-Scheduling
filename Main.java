a51ef0a331e475cd8586c67fb47a066f5edc23c6
YeohHanYi <128678869+Foreen03@users.noreply.github.com>
2025-01-26 17:09:13 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+/*
+Team Member
+1. Sia Jing Liang 1211106208
+2. Lee Xiang Ze 1211106818
+3. Yeoh Han Yi 1211106319
+4. Ngo Shun Hong 1211106586
+*/
 public class Main {
     public static void main(String[] args) {
         new Controller();    
